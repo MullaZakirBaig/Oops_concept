462c1a36ef57a3c725a96c582a27a87407764ead
zakir <zakirbaig0313@gmail.com>
2023-09-25 19:55:51 +0530

    new class

--- src/oops_12/OverRiding.java
+++ src/oops_12/OverRiding.java
@@ -1,26 +1,28 @@
 package oops_12;
 
 public class OverRiding extends add {
-	
-	public void Add(int a,int b) {
-		int c = a+b;
-		System.out.println("method of two digit "+c);
-		super.Add(20, 30);
+
+	public void Add(int a, int b) {
+		int c = a + b;
+		System.out.println("method of two digit " + c);
+
 	}
-	
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		OverRiding o = new OverRiding();
+		add o1 = new OverRiding();
 		o.Add(10, 20);
-		
+		o1.Add(20, 40);
+
 	}
 
 }
-class add{
+
+class add {
 	// method two digit
-	public void Add(int a,int b) {
-		int c = a+b;
-		System.out.println("method of two digit "+c);
+	public void Add(int a, int b) {
+		int c = a + b;
+		System.out.println("method of two digit " + c);
 	}
 }
