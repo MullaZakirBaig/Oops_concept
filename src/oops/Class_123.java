0a6141c1323f99eb420a6a0f798047c0f2953a26
Mulla Zakir Baig <144655659+MullaZakirBaig@users.noreply.github.com>
2023-09-27 14:03:38 +0530

    new ipdates for pull

--- src/oops/Class_123.java
+++ src/oops/Class_123.java
@@ -4,7 +4,7 @@ public class Class_123 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-System.out.println("zakir baig");
+System.out.println("Welcomr to all");
 	}
 
 }
