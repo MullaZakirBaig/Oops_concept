641458fbd6dde5a3b9a601956bb5d11b51c4ceac
zakir <zakirbaig0313@gmail.com>
2023-09-27 14:05:16 +0530

    add getters

--- src/oops_12/GetterSetter.java
+++ src/oops_12/GetterSetter.java
@@ -5,22 +5,22 @@ public class GetterSetter {
 	private static String UserName = "zakirbaig0313";
 	private static String Password = "zakir@baig.0313";
 
-	// Getter Method
+	// Getter Method for UserName
 	public String getUserName() {
 		return UserName;
 	}
 
-	// Getter Method
+	// Getter Method for Password
 	public String getPassword() {
 		return Password;
 	}
 
-	// Setter Method
+	// Setter Method for UserName
 	public void setUserName(String UserName) {
 		this.UserName = UserName;
 	}
 
-	// Setter Method
+	// Setter Method for Pasword
 	public void setPassword(String Password) {
 		this.Password = Password;
 	}
